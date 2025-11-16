fbd6d4da2c5d737b6f54b344844984db19c1fd5f
yashkumarsaurabh <65075425+yashkumarsaurabh@users.noreply.github.com>
2025-11-16 22:30:02 +0530

    created login panel

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,19 +1,30 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-import { FormBuilder, FormControl } from "@angular/forms";
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.html',
     styleUrls: ['./login.scss'],
-    imports: []
+    imports: [CommonModule, MatCardModule, ReactiveFormsModule, MatInputModule, MatButtonModule]
 })
 export class Login {
     loginForm;
 
     constructor(private fb: FormBuilder) {
         this.loginForm = this.fb.group({
-            username: new FormControl(''),
-            password: new FormControl('')
+            email: new FormControl('', [Validators.required, Validators.email]),
+            password: new FormControl('', Validators.required)
         })
+    }
+
+    onSubmit() {
+        if(this.loginForm.valid){
+            const {email, password} = this.loginForm.value
+            console.log("email: ", email, password)
+        }
     }
 }
