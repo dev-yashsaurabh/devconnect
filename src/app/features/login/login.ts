90d039a19f3a2ef5d8494d0f33a8f6787bdbc836
yashkumarsaurabh <65075425+yashkumarsaurabh@users.noreply.github.com>
2025-11-16 23:18:58 +0530

    add auth

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatInputModule } from "@angular/material/input";
+import { AuthService } from "../../core/auth/auth.service";
 
 @Component({
     selector: 'app-login',
@@ -13,18 +14,21 @@ import { MatInputModule } from "@angular/material/input";
 })
 export class Login {
     loginForm;
+    auth = inject(AuthService);
 
     constructor(private fb: FormBuilder) {
         this.loginForm = this.fb.group({
-            email: new FormControl('', [Validators.required, Validators.email]),
-            password: new FormControl('', Validators.required)
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required]
         })
     }
 
     onSubmit() {
         if(this.loginForm.valid){
-            const {email, password} = this.loginForm.value
-            console.log("email: ", email, password)
+            const {email, password} = this.loginForm.value;
+            if(email && password) {
+                this.auth.login(email, password);
+            }
         }
     }
 }
